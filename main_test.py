744bba5590e645d769e2641a6d189cfda8f2c7c8
Andrew Henning-Kolberg <Andrew.Kolberg@gmail.com>
2018-07-06 22:54:13 -0400

    add git ignore, first test

--- main_test.py
+++ main_test.py
@@ -4,3 +4,6 @@ import main
 class Test_main(unittest.TestCase):
     def test_known_answers(self):
         self.assertEqual(main.hello(), "Hello, World!")
+        
+if __name__ == '__main__':
+    unittest.main()
