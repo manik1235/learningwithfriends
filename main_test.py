923072cf2c7f07ce10d9c5e679862427d8ab7863
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2018-07-07 23:18:41 +0000

    not sure what changed in main_test.py

--- main_test.py
+++ main_test.py
@@ -2,8 +2,13 @@ import unittest
 import main
 
 class Test_main(unittest.TestCase):
-    def test_known_answers(self):
+    def test_hello_world(self):
         self.assertEqual(main.hello(), "Hello, World!")
+
+    def test_goodbye_name(self):
+        names = ['Daniel', 'Joey', 'Jim-Bob']
+        for name in names:
+            self.assertEqual(main.goodbye(name), "Goodbye, {}. We'll miss you!".format(name))
         
 if __name__ == '__main__':
     unittest.main()
